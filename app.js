2d219921803cb755e1b35425f0c95cfcd682bec9
wujunhua <wujunhua@uinnova.com>
2024-10-29 11:18:59 +0800

    bulid: test ws

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
+const WebSocket = require('ws');
 const indexRouter = require('./routes/index');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,6 +17,9 @@ app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
   });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
+
+// Create WebSocket server attached to http server
+const wss = new WebSocket.Server({ server });
