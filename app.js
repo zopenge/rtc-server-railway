3acdacb635d9a52a635d454d334ba02085643345
wujunhua <wujunhua@uinnova.com>
2024-10-29 11:41:08 +0800

    build: strange

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-// const WebSocket = require('ws');
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -17,9 +16,6 @@ app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
   });
 
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
-
-// Create WebSocket server attached to http server
-//const wss = new WebSocket.Server({ server });
