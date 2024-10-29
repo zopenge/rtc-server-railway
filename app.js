e91163c7cfbb91b70b427b9b3caed7af58d4f82b
wujunhua <wujunhua@uinnova.com>
2024-10-29 11:44:55 +0800

    bulid: process can not use

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const WebSocket = require('ws');
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -14,8 +15,11 @@ app.use('/', indexRouter);
 // Catch-all route for handling 404 errors
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  });
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
 });
+
+const server = app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+// Create WebSocket server attached to http server
+const wss = new WebSocket.Server({ server });
