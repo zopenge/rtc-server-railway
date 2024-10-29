a0638f6e01cf3a1ad093b205e0a33456caeb9f30
wujunhua <wujunhua@uinnova.com>
2024-10-29 11:14:19 +0800

    build: revert

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const path = require('path');
-const WebSocket = require('ws');
 const indexRouter = require('./routes/index');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,30 +14,8 @@ app.use('/', indexRouter);
 // Catch-all route for handling 404 errors
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+  });
 
-// Create http server
-const server = app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
-//    console.log(`Server running at http://${require('os').hostname()}:${PORT}/`);
-});
-
-// Create WebSocket server attached to http server
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', function connection(ws) {
-    console.log('New client connected');
-    
-    ws.on('message', function incoming(message) {
-        // Broadcast message to all connected clients except sender
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
-    });
-
-    ws.on('close', function() {
-        console.log('Client disconnected');
-    });
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
