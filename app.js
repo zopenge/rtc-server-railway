cac0686b9844a587d170ac5222f832febb702607
wujunhua <wujunhua@uino.com>
2024-11-02 16:44:27 +0800

    build: fix mem leak for container

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const session = require("express-session");
+const PGStore = require('connect-pg-simple')(session);
+const { Pool } = require('pg');
+const crypto = require('crypto');
 const http = require('http');
 const config = require('./config');
 const setupMiddleware = require('./middleware');
 const setupRoutes = require('./routes');
-const authService = require('./services/supabase/auth');
 
 // create express app and http server
 const app = express();
 const server = http.createServer(app);
+
+const pool = new Pool({
+    connectionString: 'your-supabase-connection-string',
+});
+
+const secret = crypto.randomBytes(32).toString('hex');
+
+app.use(session({
+    store: new PGStore({
+        pool: pool,
+    }),
+    secret: secret,
+    resave: false,
+    saveUninitialized: false,
+}));
 
 // setup middleware
 setupMiddleware(app);
